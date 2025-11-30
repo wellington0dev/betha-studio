c61fc50fb6a98c53bc147f0976964dc651d1d488
Your Name <wellingtonalexandre857@gmail.com>
2025-11-30 16:34:57 -0300

    create auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { ClientSection } from './pages/client-section/client-section';
 import { Auth } from './pages/auth/auth';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: 'home', component: Home },
-    { path: 'client', component: ClientSection },
+    { path: 'home', component: Home, canActivate:[AuthGuard] },
+    { path: 'client', component: ClientSection, canActivate:[AuthGuard] },
     { path: 'auth', component: Auth},
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
