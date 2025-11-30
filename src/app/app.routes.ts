ce0ac41a298d43c0093d5560cc275587706ff205
Your Name <wellingtonalexandre857@gmail.com>
2025-11-30 16:35:17 -0300

    create client section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Auth } from './pages/auth/auth';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: 'home', component: Home, canActivate:[AuthGuard] },
+    { path: 'home', component: Home, canActivate:[] },
     { path: 'client', component: ClientSection, canActivate:[AuthGuard] },
     { path: 'auth', component: Auth},
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
